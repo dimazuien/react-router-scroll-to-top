d28dd6e7cb0ab747cfc3b537331d647e60534476
Dima Zuien <dimazuien@gmail.com>
2021-11-28 22:53:10 +0200

    chore: add minor organizational fixes

--- tests/ScrollToTop.test.tsx
+++ tests/ScrollToTop.test.tsx
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
+
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React, { useCallback } from 'react';
@@ -9,6 +10,7 @@ import {
   Switch,
   useHistory,
 } from 'react-router-dom';
+
 import ScrollToTop from '../src/ScrollToTop';
 
 describe('ScrollToTop', () => {
